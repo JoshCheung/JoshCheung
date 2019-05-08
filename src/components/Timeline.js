809ca29fdc9c5e665bbf5e24cbf0cce6cd8ac1db
Josh <JTCheung3@gmail.com>
2019-05-08 10:46:15 -0700

    edits

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -30,20 +30,10 @@ export default function Timeline({ title, subtitle, dark, id, image}) {
                                 who expanded my knowledge academically, creatively, and mentally. Through in-class experiences and
                                 group projects, I have been exposed to a variety of languages, environments, and the application
                                 development process that have given me a greater understanding for the different aspects of
-                                computer science. My peers have inspired me through own drive to learn and make an impact doing something they love to open 
+                                computer science. My peers have inspired me through their own drive to learn and make an impact doing something they love to open
                                 myself to new opportunities and grasp them to make something of my own.
                                 UC Santa Cruz has given me treasurable friendships, shown me my passion for computer science, and
                                 allowed me to do what I love -learn."
-                            />
-                            <Card
-                                title="Septemeber 2017 to March 2019"
-                                image={resnet}
-                                subtitle="Information Technician"
-                                text="ResNet was I place where I had to effectively communicate with students and faculty members
-                                in order to resolve their technical issues. I had to hone these communications skills over the phone,
-                                online, or in person to assist these students. Working at ResNet, I configured routers to be compliant
-                                with the campus network, took apart computers in order to fix them, and removed malware/ viruses to
-                                protect clients’ data."
                             />
                             <Card
                                 title="October 2018"
@@ -54,6 +44,17 @@ export default function Timeline({ title, subtitle, dark, id, image}) {
                                 Ansible, unit testing different call functions on Webex, and working with mySQL databases. Here, I
                                 gained confidence in my abilities to write code, have high level discussions in meetings, and overall
                                 as a software engineer."
+                            />
+
+                            <Card
+                                title="September 2017 to March 2019"
+                                image={resnet}
+                                subtitle="Information Technician"
+                                text="ResNet was I place where I had to effectively communicate with students and faculty members
+                                in order to resolve their technical issues. I had to hone these communications skills over the phone,
+                                online, or in person to assist these students. Working at ResNet, I configured routers to be compliant
+                                with the campus network, took apart computers in order to fix them, and removed malware/ viruses to
+                                protect clients’ data."
                             />
                         </div>
                     </div>
